db519897e10fd16140046ab03073eb3d5caa6450
myciek00 <m_rychel@op.pl>
2017-12-07 23:10:41 +0100

    fixed issues with native - code is messy, plenty of state

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-import $ from 'jquery'; 
+import $ from 'jquery';
 import getItems from './mock-server';
+import { window } from 'rxjs/operator/window';
 
 const $search = $('#search');
-const $list   = $('#list');
+const $list = $('#list');
+
+// issues with previous code:
+// - racing
+// - 2. calling remote server with every keystroke
+// - 3. calling remote server event if value is not changed, e.g. using up/down arrows inside input
+
+let lastQuery = null;
+let lastTimeout = null;
+let nextQueryId = 0;
 
 $search.on('keyup', e => {
- const search = e.target.value;
- getItems(search)
-  .then(data => {
-    $list.empty();
-    const $items = data.map(item => $('<li />').text(item));
-    $list.append($items);
-  });
+  const search = e.target.value;
+
+  // fixed .3
+  if (search === lastQuery) {
+    return;
+  }
+  lastQuery = search;
+
+  // fixed .2
+  if (lastTimeout) {
+    window.clearTimeout(lastQuery);
+  }
+
+  let queryId = ++nextQueryId;
+
+  lastTimeout = window.setTimeout(() => {
+
+    getItems(search)
+      .then(data => {
+        
+        // fixed .1
+        if(queryId !== nextQueryId) {
+          return;
+        }
+        //
+
+        $list.empty();
+        const $items = data.map(item => $('<li />').text(item));
+        $list.append($items);
+
+      });
+
+  }, 500);
+
 });
 
