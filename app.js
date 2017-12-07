9fec3e36d1a29a38076fb455480bd5f2362b63b9
myciek00 <m_rychel@op.pl>
2017-12-07 23:27:16 +0100

    rxjs

--- app.js
+++ app.js
@@ -1,53 +1,18 @@
 import $ from 'jquery';
+import Rx from 'rxjs/Rx';
+
 import getItems from './mock-server';
-import { window } from 'rxjs/operator/window';
 
 const $search = $('#search');
 const $list = $('#list');
 
-// issues with previous code:
-// - racing
-// - 2. calling remote server with every keystroke
-// - 3. calling remote server event if value is not changed, e.g. using up/down arrows inside input
-
-let lastQuery = null;
-let lastTimeout = null;
-let nextQueryId = 0;
-
-$search.on('keyup', e => {
-  const search = e.target.value;
-
-  // fixed .3
-  if (search === lastQuery) {
-    return;
-  }
-  lastQuery = search;
-
-  // fixed .2
-  if (lastTimeout) {
-    window.clearTimeout(lastQuery);
-  }
-
-  let queryId = ++nextQueryId;
-
-  lastTimeout = window.setTimeout(() => {
-
-    getItems(search)
-      .then(data => {
-        
-        // fixed .1
-        if(queryId !== nextQueryId) {
-          return;
-        }
-        //
-
-        $list.empty();
-        const $items = data.map(item => $('<li />').text(item));
-        $list.append($items);
-
-      });
-
-  }, 500);
-
-});
-
+Rx.Observable.fromEvent($search, 'keyup')
+  .map(e => e.target.value) // transform jQuery events to input text
+  .distinctUntilChanged() // operator that produces value only if the prev values has changed
+  .debounceTime(250) // operator that limits produces values to only one value per give time period
+  .switchMap(search => getItems(search)) // passing promise to switchMap (flatMapLatest, discards old data) operator
+  .subscribe(data => {
+    $list.empty();
+    const $items = data.map(item => $('<li />').text(item));
+    $list.append($items);  
+  });
